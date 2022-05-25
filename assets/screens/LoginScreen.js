1dfd7506a4b25a7fa82fad015602f18d9e35475f
Germaine Lee <germainelee02@gmail.com>
2022-05-25 22:03:30 +0800

    nicer input buttons, with eye icons

--- assets/screens/LoginScreen.js
+++ assets/screens/LoginScreen.js
@@ -1,103 +1,155 @@
-import React, { useState, useEffect } from 'react';
-import { View, SafeAreaView, StyleSheet, Text, Image, TouchableOpacity, TextInput, KeyboardAvoidingView} from 'react-native' ;
-import { authentication } from '../../firebase/firebase-config';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
+import { authentication } from "../../firebase/firebase-config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const LoginScreen = ({ navigation }) => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [canSee, setCanSee] = useState(true);
+  const [icon, setIcon] = useState("eye");
 
   // logs in to the homescreen when the authentication state is changed (log in or sign up)
   // not really sure how this works..
   useEffect(() => {
-    const unsubscribe = authentication.onAuthStateChanged(user => {
+    const unsubscribe = authentication.onAuthStateChanged((user) => {
       if (user) {
-        navigation.navigate('temp home');
+        navigation.navigate("temp home");
       }
-    })
-    return unsubscribe
-  }, [])
+    });
+    return unsubscribe;
+  }, []);
 
   // handles log in with firebase
   const handleLogin = () => {
     signInWithEmailAndPassword(authentication, email, password)
-    .then(userCredentials => {
-      const user = userCredentials.user;
-      console.log(user.email);
-    })
-    .catch(error => alert(error.message));
-  }
-    return (  
-      <KeyboardAvoidingView style = {styles.background} >
-          <TouchableOpacity style = {styles.signUp} onPress = {() => navigation.navigate('signup')}>
-              <Text style = {styles.signUpText}> Sign Up </Text>
-          </TouchableOpacity>
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log(user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
 
-          <Image style = {styles.image} source = {require('../bee-logo.png')} />
-          <TextInput 
-            placeholder = 'Email...' 
-            autoCapitalize = 'none' 
-            value = { email }
-            onChangeText = {text => setEmail(text)}
-            style = {styles.emailBox}/>
-          <TextInput
-           placeholder = 'Password...' 
-           secureTextEntry = {true} 
-           value = { password }
-           onChangeText = {pass => setPassword(pass)}
-           autoCapitalize = 'none' 
-           style = {styles.emailBox}/>
-          <TouchableOpacity 
-            style = {styles.loginButton} 
-            onPress = {() => handleLogin()}>
-              <Text style = {styles.loginText}> Login </Text>
-          </TouchableOpacity>
-      </KeyboardAvoidingView>
-    );
-}
+  const onIconPress = () => {
+    let iconName = canSee ? "eye-off" : "eye";
+    setCanSee(!canSee);
+    setIcon(iconName);
+  };
+  return (
+    <KeyboardAvoidingView style={styles.background}>
+      <TouchableOpacity
+        style={styles.signUp}
+        onPress={() => navigation.navigate("signup")}
+      >
+        <Text style={styles.signUpText}> Sign Up </Text>
+      </TouchableOpacity>
+
+      <Image style={styles.image} source={require("../bee-logo.png")} />
+      <TextInput
+        placeholder="Email..."
+        autoCapitalize="none"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        style={styles.emailBox}
+      />
+
+      <View style={styles.passwordContainer}>
+        <TextInput
+          placeholder="Password..."
+          secureTextEntry={canSee}
+          value={password}
+          onChangeText={(pass) => setPassword(pass)}
+          autoCapitalize="none"
+          style={styles.passwordText}
+        />
+
+        <TouchableOpacity onPress={() => onIconPress()}>
+          <MaterialCommunityIcons name={icon} size={25} />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity
+        style={styles.loginButton}
+        onPress={() => handleLogin()}
+      >
+        <Text style={styles.loginText}> Login </Text>
+      </TouchableOpacity>
+
+      {/* remove this after done */}
+      <Button
+        title="direct shortcut to home page (delete this after done)"
+        onPress={() => navigation.navigate("temp home")}
+      />
+    </KeyboardAvoidingView>
+  );
+};
 
 const styles = StyleSheet.create({
-    background: {   
-        flex: 1,
-        backgroundColor: 'rgba(245, 233, 188, 1)',
-        justifyContent: 'center',
-        alignItems: 'center'
-      },
-      emailBox: {
-          height: 70,
-          width: '100%',
-          backgroundColor: 'white',
-          margin: 5,
-          borderRadius: 15
-      },
-      image: {
-        height: '50%',
-        width: '50%',
-        margin: 0
-    },
-      loginButton: {
-          width: '50%',
-          height: 70,
-          backgroundColor: 'rgb(251, 194, 8)',
-           justifyContent: 'center',
-           alignItems: 'center',
-           borderRadius: 100,
-           margin: 20,
-      },
-      loginText: {
-          fontSize: 18,
-          fontWeight: 'bold'
-      },
-      signUpText: {
-        textDecorationLine: 'underline', 
-        fontSize: 18
-      },
-      signUp: {
-        position: 'absolute',
-        top: 85, 
-        right: 30, 
-      }
-})
+  background: {
+    flex: 1,
+    backgroundColor: "rgba(245, 233, 188, 1)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  emailBox: {
+    height: 70,
+    width: "95%",
+    backgroundColor: "white",
+    margin: 10,
+    padding: 10,
+    borderRadius: 15,
+  },
+  image: {
+    height: "50%",
+    width: "50%",
+    margin: 0,
+  },
+  loginButton: {
+    width: "50%",
+    height: 70,
+    backgroundColor: "rgb(251, 194, 8)",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    margin: 20,
+  },
+  loginText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  passwordContainer: {
+    flexDirection: "row",
+    margin: 10,
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 10,
+    height: 70,
+    width: "95%",
+  },
+  passwordText: {
+    flex: 1,
+  },
+  signUpText: {
+    textDecorationLine: "underline",
+    fontSize: 18,
+  },
+  signUp: {
+    position: "absolute",
+    top: 85,
+    right: 30,
+  },
+});
 
 export default LoginScreen;
--- assets/screens/SignUpScreen.js
+++ assets/screens/SignUpScreen.js
@@ -1,96 +1,157 @@
-import React, {useState} from 'react';
-import { Alert, View, SafeAreaView, StyleSheet, Text, Image, TouchableOpacity, TextInput, KeyboardAvoidingView} from 'react-native' ;
-import { authentication } from '../../firebase/firebase-config';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from "react";
+import {
+  Alert,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
+import { authentication } from "../../firebase/firebase-config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 function SignUpScreen(props) {
   // the email that is saved and used to log in
-    const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
-    // the password that comes with the email 
-    const [password, setPassword] = useState('');
+  // the password that comes with the email
+  const [password, setPassword] = useState("");
 
-    // asking users to enter password a second time to check if it matches with the password typed in the second time
-    const [repassword, setRepassword] = useState('');
+  // asking users to enter password a second time to check if it matches with the password typed in the second time
+  const [repassword, setRepassword] = useState("");
 
-    // function that uses firebase to sign up for an account
-    const handleSignUp = () => {
-      if (password == repassword) {
-        createUserWithEmailAndPassword(authentication, email, password)
-         .then(userCredentials => {
-           const user = userCredentials.user;
-           console.log(user.email);
-         })
-         .catch(error => alert(error.message));
-      } else {
-        Alert.alert('Error', 'Password mismatch');
-      }
-      }
-    return (    
-      <KeyboardAvoidingView style = {styles.background} >
-          <Image style = {styles.image} source = {require('../bee-logo.png')} />
-          <TextInput 
-          placeholder = 'Email...'
-          // when the user types in, it changes the `email` state at the top
-          onChangeText={text => setEmail(text)}
-           autoCapitalize = 'none' 
-           style = {styles.emailBox}/>
-          <TextInput 
-          placeholder = 'Password...' 
-          secureTextEntry = { true } 
+  const [canSeePass, setCanSeePass] = useState(true);
+
+  const [iconPass, setIconPass] = useState("eye");
+
+  const [canSeeRepass, setCanSeeRepass] = useState(true);
+
+  const [iconRepass, setIconRepass] = useState("eye");
+
+  const onPassIconPress = () => {
+    let iconName = canSeePass ? "eye-off" : "eye";
+    setCanSeePass(!canSeePass);
+    setIconPass(iconName);
+  };
+
+  const onRepassIconPress = () => {
+    let iconName = canSeeRepass ? "eye-off" : "eye";
+    setCanSeeRepass(!canSeeRepass);
+    setIconRepass(iconName);
+  };
+  // function that uses firebase to sign up for an account
+  const handleSignUp = () => {
+    if (password == repassword) {
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((userCredentials) => {
+          const user = userCredentials.user;
+          console.log(user.email);
+        })
+        .catch((error) => alert(error.message));
+    } else {
+      Alert.alert("Error", "Password mismatch");
+    }
+  };
+  return (
+    <KeyboardAvoidingView style={styles.background}>
+      <Image style={styles.image} source={require("../bee-logo.png")} />
+      <TextInput
+        placeholder="Email..."
+        // when the user types in, it changes the `email` state at the top
+        onChangeText={(text) => setEmail(text)}
+        autoCapitalize="none"
+        style={styles.emailBox}
+      />
+      <View style={styles.passwordContainer}>
+        <TextInput
+          placeholder="Password..."
+          secureTextEntry={canSeePass}
           // when user types in the password, it changes `password` state at the top
-          onChangeText={text => setPassword(text)}
-          autoCapitalize = 'none' 
-          style = {styles.emailBox}/>
-          <TextInput 
-          placeholder = 'Re-enter password...' 
+          onChangeText={(text) => setPassword(text)}
+          autoCapitalize="none"
+          style={styles.passwordText}
+        />
+
+        <TouchableOpacity onPress={() => onPassIconPress()}>
+          <MaterialCommunityIcons name={iconPass} size={25} />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.passwordContainer}>
+        <TextInput
+          placeholder="Re-enter password..."
           // changes `repassword` state at the top when user types in repassword
-          secureTextEntry = { true } autoCapitalize = 'none' 
-          onChangeText={text => setRepassword(text)}
-          style = {styles.emailBox}/>
-          <TouchableOpacity
-           style = {styles.loginButton} 
-           // when button is pressed, signs up
-           onPress = {() => handleSignUp()}>
-              <Text style = {styles.loginText}> Sign up! </Text>
-          </TouchableOpacity>
-      </KeyboardAvoidingView>
-    );
+          secureTextEntry={canSeeRepass}
+          autoCapitalize="none"
+          onChangeText={(text) => setRepassword(text)}
+          style={styles.passwordText}
+        />
+
+        <TouchableOpacity onPress={() => onRepassIconPress()}>
+          <MaterialCommunityIcons name={iconRepass} size={25} />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        style={styles.loginButton}
+        // when button is pressed, signs up
+        onPress={() => handleSignUp()}
+      >
+        <Text style={styles.loginText}> Sign up! </Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-    background: {   
-        flex: 1,
-        backgroundColor: 'rgba(245, 233, 188, 1)',
-        justifyContent: 'center',
-        alignItems: 'center'
-      },
-      emailBox: {
-          height: 70,
-          width: '100%',
-          backgroundColor: 'white',
-          margin: 5,
-          borderRadius: 15
-      },
-      image: {
-        height: '50%',
-        width: '50%',
-        margin: 0
-    },
-      loginButton: {
-          width: '50%',
-          height: 70,
-          backgroundColor: 'rgb(251, 194, 8)',
-           justifyContent: 'center',
-           alignItems: 'center',
-           borderRadius: 100,
-           margin: 20,
-      },
-      loginText: {
-          fontSize: 18,
-          fontWeight: 'bold'
-      },
-
-})
+  background: {
+    flex: 1,
+    backgroundColor: "rgba(245, 233, 188, 1)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  emailBox: {
+    height: 70,
+    width: "95%",
+    backgroundColor: "white",
+    margin: 10,
+    borderRadius: 15,
+    padding: 10,
+  },
+  image: {
+    height: "50%",
+    width: "50%",
+    margin: 0,
+  },
+  loginButton: {
+    width: "50%",
+    height: 70,
+    backgroundColor: "rgb(251, 194, 8)",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    margin: 20,
+  },
+  loginText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  passwordContainer: {
+    flexDirection: "row",
+    margin: 10,
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 10,
+    height: 70,
+    width: "95%",
+  },
+  passwordText: {
+    flex: 1,
+  },
+});
 
 export default SignUpScreen;
