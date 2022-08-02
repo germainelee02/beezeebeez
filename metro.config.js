401ea637f7193e84f18cfb7d481967422400fadb
Germaine Lee <germainelee02@gmail.com>
2022-08-02 16:09:57 +0800

    clean up ignored files

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-// something that i copy pasted from stackOF because this file is needed for firebase dependencies... but the expo CLI doesn't prepare this file for you or something...
-
-const { getDefaultConfig } = require("@expo/metro-config");
-
-const defaultConfig = getDefaultConfig(__dirname);
-
-defaultConfig.resolver.assetExts.push("cjs");
-
-module.exports = defaultConfig;
